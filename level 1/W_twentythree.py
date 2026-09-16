71b6426c05482f86e020c7c51a2b0f0054f0197f
Aman6354 <Laxmiputra7678@gmail.com>
2026-09-16 14:44:04 +0530

    Initial commit

--- level 1/W_twentythree.py
+++ level 1/W_twentythree.py
@@ -1,3 +1,12 @@
+'''Problem Statement
+
+Create these variables:
+
+product_name → "Laptop"
+price → 55000
+rating → 4.5
+available → True'''
+
 product_name = "laptop" 
 print(type(product_name ))
 price = 55000
---
+++ level 1/X_twentyfour.py
@@ -0,0 +1,17 @@
+'swaping of two numbers'
+
+a = 10
+b = 20
+temp = a
+a = b
+b = temp
+print(a,b)
+
+n=10
+s=30
+print("BEfore: ",n,s)
+n=n+s
+s=n-s
+n=n-s
+print("After: ",n,s)
+
