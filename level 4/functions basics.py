3f7d76185f94b27217d63f1076a35297ae3e0717
Aman6354 <Laxmiputra7678@gmail.com>
2026-09-24 23:52:08 +0530

    updated practice

---
+++ level 4/functions basics.py
@@ -0,0 +1,44 @@
+'''Function ek reusable block of code hota hai.
+Ek baar function bana do, phir usse baar-baar call kar sakte ho.
+
+Basic structure:
+
+def function_name():
+    # code
+
+Aur use chalane ke liye:
+
+function_name()
+
+Example:
+
+def greet():
+    print("Hello!")
+
+greet()
+greet()
+
+Output:
+
+Hello!
+Hello!
+🏆 Challenge 1 — My Introduction
+
+Objective: Apna pehla function banana.
+
+Program mein:
+
+my_intro() naam ka function banao.
+Function ke andar:
+apna naam print karo
+apni age print karo
+"I am learning Python" print karo
+Function ko 2 baar call karo.'''
+
+def my_intro():
+    print("Aman Tiwari ")
+    print("My age is 56")
+    print("I am learning Python")
+
+my_intro()
+my_intro()
